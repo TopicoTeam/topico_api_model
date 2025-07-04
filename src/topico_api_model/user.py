02614e60d9b4a036cab12b72e88e7ebc8fc15ecd
Shadrin Igor <igor@shadrins.ru>
2025-07-04 12:23:44 +0300

    init

--- src/topico_api_model/user.py
+++ src/topico_api_model/user.py
@@ -5,23 +5,7 @@ from uuid import UUID
 from pydantic import BaseModel, Field
 
 
-class UserRole(BaseModel):
-    """Role model"""
-    nickname: str
-    description: str
-
-
-class MinecraftUser(BaseModel):
-    id: UUID
-    nickname: str
-
-
-class TelegramUser(BaseModel):
-    id: int = Field(description="User telegram ID")
-
-
 class TopicoUser(BaseModel):
     id: UUID = Field(description="Internal topico team user ID")
-    minecraft: MinecraftUser | None = Field(description="User minecraft account data")
-    telegram: TelegramUser
-    role: UserRole
+    minecraft_id: UUID  | None = Field(description="User minecraft account data")
+    telegram_id: int

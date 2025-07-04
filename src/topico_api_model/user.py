eda4131744a29c3089d55e8db48b9a99f9dd66c6
Shadrin Igor <igor@shadrins.ru>
2025-07-04 12:28:22 +0300

    init

--- src/topico_api_model/user.py
+++ src/topico_api_model/user.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from logging import fatal
+from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel, Field
@@ -7,5 +8,5 @@ from pydantic import BaseModel, Field
 
 class TopicoUser(BaseModel):
     id: UUID = Field(description="Internal topico team user ID")
-    minecraft_id: UUID  | None = Field(description="User minecraft account data")
+    minecraft_id: Optional[UUID] = Field(description="User minecraft account data")
     telegram_id: int
